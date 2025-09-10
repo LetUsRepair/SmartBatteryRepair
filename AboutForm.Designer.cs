29440f62262d6e5ffc135b18a12457d1afeca287
unknown <geir@geirandersen.com>
2025-09-10 18:47:58 +0200

    Fixed a slight error in push, added all new files and name

---
+++ AboutForm.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace SmartBatteryRepair
+{
+    partial class AboutForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.VersionLabel = new System.Windows.Forms.Label();
+            this.AboutDescriptionLabel = new System.Windows.Forms.Label();
+            this.AboutTitleLabel = new System.Windows.Forms.Label();
+            this.AboutPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.AboutPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VersionLabel
+            // 
+            this.VersionLabel.AutoSize = true;
+            this.VersionLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.VersionLabel.Location = new System.Drawing.Point(364, 7);
+            this.VersionLabel.Name = "VersionLabel";
+            this.VersionLabel.Size = new System.Drawing.Size(58, 16);
+            this.VersionLabel.TabIndex = 6;
+            this.VersionLabel.Text = "Ver: N/A";
+            // 
+            // AboutDescriptionLabel
+            // 
+            this.AboutDescriptionLabel.AutoSize = true;
+            this.AboutDescriptionLabel.Location = new System.Drawing.Point(140, 35);
+            this.AboutDescriptionLabel.Name = "AboutDescriptionLabel";
+            this.AboutDescriptionLabel.Size = new System.Drawing.Size(265, 13);
+            this.AboutDescriptionLabel.TabIndex = 5;
+            this.AboutDescriptionLabel.Text = "FREE Repair tool for smart batteries, using SMBus (I²C)";
+            // 
+            // AboutTitleLabel
+            // 
+            this.AboutTitleLabel.AutoSize = true;
+            this.AboutTitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.AboutTitleLabel.Location = new System.Drawing.Point(139, 1);
+            this.AboutTitleLabel.Name = "AboutTitleLabel";
+            this.AboutTitleLabel.Size = new System.Drawing.Size(187, 24);
+            this.AboutTitleLabel.TabIndex = 4;
+            this.AboutTitleLabel.Text = "SmartBatteryRepair";
+            // 
+            // AboutPictureBox
+            // 
+            this.AboutPictureBox.BackgroundImage = global::SmartBatteryRepair.Properties.Resources.battery_icon;
+            this.AboutPictureBox.Location = new System.Drawing.Point(5, 5);
+            this.AboutPictureBox.Name = "AboutPictureBox";
+            this.AboutPictureBox.Size = new System.Drawing.Size(128, 128);
+            this.AboutPictureBox.TabIndex = 7;
+            this.AboutPictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(141, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Developer: (@LetUsRepair)";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(142, 71);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(164, 13);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/LetUsRepair/";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Initial development called SmartBatteryHack;";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(141, 123);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(245, 13);
+            this.linkLabel2.TabIndex = 11;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "https://github.com/laszlodaniel/SmartBatteryHack";
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(140, 84);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(203, 13);
+            this.linkLabel3.TabIndex = 12;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "(https://buymeacoffee.com/LetUsRepair)";
+            // 
+            // AboutForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 141);
+            this.Controls.Add(this.linkLabel3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.AboutPictureBox);
+            this.Controls.Add(this.VersionLabel);
+            this.Controls.Add(this.AboutDescriptionLabel);
+            this.Controls.Add(this.AboutTitleLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AboutForm";
+            this.Text = "About";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.AboutForm_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.AboutPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label VersionLabel;
+        private System.Windows.Forms.Label AboutDescriptionLabel;
+        private System.Windows.Forms.Label AboutTitleLabel;
+        private System.Windows.Forms.PictureBox AboutPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+    }
+}
